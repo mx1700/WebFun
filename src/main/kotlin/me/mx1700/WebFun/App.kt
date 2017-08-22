038d9c4d2a7b9855b98e9fc23f54d82b4e47d0fb
lizhaoguang <lizhaoguang@dangdang.com>
2017-08-22 19:59:21 +0800

    增加 jetty 依赖

--- src/main/kotlin/me/mx1700/WebFun/App.kt
+++ src/main/kotlin/me/mx1700/WebFun/App.kt
@@ -1,16 +1,45 @@
 package me.mx1700.WebFun
-
 import me.mx1700.WebFun.Annotations.Get
+import org.eclipse.jetty.server.Request
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.AbstractHandler
+import org.eclipse.jetty.server.handler.gzip.GzipHandler
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 fun main(args: Array<String>) {
     val app = Application.getApp().run()
-    app.testRoute("GET", "/", mapOf("name" to "hahaha"))
+//    app.testRoute("GET", "/", mapOf("name" to "hahaha"))
+    val server = Server(8080)
+    val handle = HttpHandler({ req, res -> })
+    server.handler = handle
+    server.start()
+    server.dumpStdErr()
+    server.join()
+}
+
+class HttpHandler(private val run: (request: HttpServletRequest,
+                                    response: HttpServletResponse) -> Unit) : AbstractHandler() {
+
+    @Throws(IOException::class, ServletException::class)
+    override fun handle(target: String,
+                        baseRequest: Request,
+                        request: HttpServletRequest,
+                        response: HttpServletResponse) {
+        val ctx = request.startAsync()
+        response.contentType = "text/html; charset=utf-8"
+        response.status = HttpServletResponse.SC_NOT_FOUND
+        run(request, response)
+
+        baseRequest.isHandled = true
+    }
 }
 
 @Get(path = "/")
-fun index(name: String = "Tom", id: Int, bean: TestBean): String {
-    bean.hello()
-    return "hello, $name; id: $id"
+fun index(name: String): String {
+    return "hello, $name"
 }
 
 
