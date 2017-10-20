2d1627551110468365e166e33cb461412bffa04b
mx1700 <mx1700@gmail.com>
2017-10-20 21:08:16 +0800

    删除无用代码

--- src/main/kotlin/me/mx1700/WebFun/Functions.kt
+++ src/main/kotlin/me/mx1700/WebFun/Functions.kt
@@ -1,9 +1,5 @@
 package me.mx1700.WebFun
 
-infix fun <T> T?.or(v: T): T {
-    return if (this == null) v else this
-}
-
 inline fun timer(action: () -> Unit, count: Int) {
     val startTime = System.currentTimeMillis()   //获取开始时间
     for (i in 1..count) {
